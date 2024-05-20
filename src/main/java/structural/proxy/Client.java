c1231f0d190bb0a2f08319a7af1884656d49eea3
Stanislav Latsko <01latsko10@gmail.com>
2024-05-20 19:26:09 +0200

    commit #14 - proxy

---
+++ src/main/java/structural/proxy/Client.java
@@ -0,0 +1,15 @@
+package structural.proxy;
+
+
+public class Client {
+
+	public static void main(String[] args) {
+		Image img = ImageFactory.getImage("A1.bmp");
+
+		img.setLocation(new Point2D(20, 10));
+		System.out.println("Image location: " + img.getLocation());
+		System.out.println("Rendering image now......");
+		img.render();
+	}
+
+}
