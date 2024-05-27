ead071fc12ece70fef22b58eae9f6a731f956448
Stanislav Latsko <01latsko10@gmail.com>
2024-05-27 19:30:27 +0200

    commit #24 - visitor

---
+++ src/main/java.latsko/behavioral/visitor/Visitor.java
@@ -0,0 +1,12 @@
+package behavioral.visitor;
+
+public interface Visitor {
+
+    void visit(Programmer programmer);
+
+    void visit(ProjectLead projectLead);
+
+    void visit(Manager manager);
+
+    void visit(VicePresident vicePresident);
+}
